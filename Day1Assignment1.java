0c1ba9948ef12fdf98470ab34db51edc08700f1d
Poorni2829 <81229284+Poorni2829@users.noreply.github.com>
2025-05-14 23:29:28 +0530

    Add files via upload

---
+++ Day1Assignment1.java
@@ -0,0 +1,39 @@
+package week1day1class;
+
+public class Day1Assignment1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+//		int Votingage= 18;
+//		
+//		if (Votingage>18) {
+//			 System.out.println("Not correct");
+//		}
+//		else if (Votingage==18) {
+//			  System.out.println("correct");
+//		}
+//		
+//		else {
+//			 System.out.println("something");
+//		}
+//			
+//		}
+	
+		
+     int value=10;
+		
+		if(value>0) {
+			
+			System.out.println("Positive Number");
+		}
+		else {
+			
+			System.out.println("Negative Number");
+		}
+	}
+
+		
+		
+		
+}
---
+++ Day1assignment2.java
@@ -0,0 +1,19 @@
+package week1day1class;
+
+public class Day1assignment2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		 Float Version =91.0f ;
+		 String Value ="Google";
+		 Boolean beta = false;
+		 int year =2008;
+		 char shortcutkey ='C';
+		 
+		 System.out.println("Version :" + Version +"\n" +"Value :"+ Value +"\n" +"beta :" +beta +"\n"+"year :"+year +"\n" +"shortcutkey :"+shortcutkey);
+		 
+		 
+	}
+
+}
